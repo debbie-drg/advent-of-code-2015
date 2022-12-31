c279f76048f6e4bc9fc4409c6d87ffa8bcb86b96
Deivi Dragon <deivi.dragon.9@gmail.com>
2022-12-31 11:07:56 +0100

    Day 8 done

---
+++ Day8/day8.py
@@ -0,0 +1,25 @@
+import sys
+
+EMPTY_STRING_SIZE = sys.getsizeof("")
+
+
+def in_memory_size(string_line: str):
+    return len(string_line) - len(eval(string_line))
+
+def encoded_size(string_line: str):
+    return 2 + string_line.count('\\') + string_line.count('"')
+
+
+
+if __name__ == "__main__":
+    try:
+        file_name = sys.argv[1]
+    except IndexError:
+        file_name = "input.txt"
+    strings = open(file_name).read().strip().split("\n")
+
+    result = sum(map(in_memory_size, strings))
+    print(f"The literal minus in memory number of characters is {result}.")
+
+    result_2 = sum(map(encoded_size, strings))
+    print(f"The encoded size minus the initial size is {result_2}.")
